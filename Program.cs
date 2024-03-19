6b0792d7f8c1099797e47d5cd781240ae6c1f9d6
Nick Templeton <nick_templeton@trekbikes.com>
2024-03-19 17:44:28 -0500

    Better looking output

--- Program.cs
+++ Program.cs
@@ -16,17 +16,39 @@ Team[] RoundWinners(Team[] teams) {
 
     for (int i = 0; i < teams.Length / 2; i++) {
         winners[i] = WhoWins(teams[i], teams[^(i + 1)]);
-        Console.WriteLine($"Seed: {teams[i]}");
-        Console.WriteLine($" vs.\tWinner: {winners[i]}");
-        Console.WriteLine($"Seed: {teams[^(i + 1)]}");
+        Console.WriteLine($"{teams[i]}");
+        Console.WriteLine($"  vs.\tWinner: {winners[i]}");
+        Console.WriteLine($"{teams[^(i + 1)]}");
     }
 
     return winners;
 }
 
-Team[] BracketWinner(Team[] teams) {
-    Console.WriteLine($"\nRound of {teams.Length * 4}");
+Team[] BracketWinner(Team[] teams, bool finalFour = false) {
+    switch (teams.Length) {
+        case 4:
+            if (finalFour) {
+                Console.WriteLine($"\nFinal Four:");
+            } else {
+                Console.WriteLine($"\nSweet Sixteen:");
+            }
+            break;
+
+        case 2:
+            if (finalFour) {
+                Console.WriteLine($"\nChampionship:");
+            } else {
+                Console.WriteLine($"\nElite Eight:");
+            }
+            break;
+
+        default:
+            Console.WriteLine($"\nRound of {teams.Length * 4}");
+            break;
+    }
+
     Console.WriteLine("-----------");
+    
     var winner = RoundWinners(teams);
 
     // If we're down to 1 we've found our winner
@@ -34,89 +56,89 @@ Team[] BracketWinner(Team[] teams) {
         return teams;
     }
 
-    return BracketWinner(winner);
+    return BracketWinner(winner, finalFour);
 }
 
 var regional1Teams = new Team[] {
-    new Team("Regional One", 1),
-    new Team("Regional One", 2),
-    new Team("Regional One", 3),
-    new Team("Regional One", 4),
-    new Team("Regional One", 5),
-    new Team("Regional One", 6),
-    new Team("Regional One", 7),
-    new Team("Regional One", 8),
-    new Team("Regional One", 9),
-    new Team("Regional One", 10),
-    new Team("Regional One", 11),
-    new Team("Regional One", 12),
-    new Team("Regional One", 13),
-    new Team("Regional One", 14),
-    new Team("Regional One", 15),
-    new Team("Regional One", 16),
+    new Team("South Carolina", 1),
+    new Team("Notre Dame", 2),
+    new Team("Oregon St", 3),
+    new Team("Indiana", 4),
+    new Team("Oklahoma", 5),
+    new Team("Nebraska", 6),
+    new Team("Ole Miss", 7),
+    new Team("North Carolina", 8),
+    new Team("Michigan St", 9),
+    new Team("Marquette", 10),
+    new Team("Texas A&M", 11),
+    new Team("FGCU", 12),
+    new Team("Fairfield", 13),
+    new Team("E Washington", 14),
+    new Team("Kent State", 15),
+    new Team("SHU/PRES", 16),
 };
 var regional1Winner = BracketWinner(regional1Teams)[0];
 Console.WriteLine($"Regional One Winner: {regional1Winner}");
 
 var regional2Teams = new Team[] {
-    new Team("Regional Two", 1),
-    new Team("Regional Two", 2),
-    new Team("Regional Two", 3),
-    new Team("Regional Two", 4),
-    new Team("Regional Two", 5),
-    new Team("Regional Two", 6),
-    new Team("Regional Two", 7),
-    new Team("Regional Two", 8),
-    new Team("Regional Two", 9),
-    new Team("Regional Two", 10),
-    new Team("Regional Two", 11),
-    new Team("Regional Two", 12),
-    new Team("Regional Two", 13),
-    new Team("Regional Two", 14),
-    new Team("Regional Two", 15),
-    new Team("Regional Two", 16),
+    new Team("Iowa", 1),
+    new Team("UCLA", 2),
+    new Team("LSU", 3),
+    new Team("Kansas St", 4),
+    new Team("Colorado", 5),
+    new Team("Louisville", 6),
+    new Team("Creighton", 7),
+    new Team("West Virginia", 8),
+    new Team("Princeton", 9),
+    new Team("UNLV", 10),
+    new Team("MTSU", 11),
+    new Team("Drake", 12),
+    new Team("Portland", 13),
+    new Team("Rice", 14),
+    new Team("CA Baptist", 15),
+    new Team("HC/UTM", 16),
 };
 var regional2Winner = BracketWinner(regional2Teams)[0];
 Console.WriteLine($"Regional Two Winner: {regional2Winner}");
 
 var regional3Teams = new Team[] {
-    new Team("Regional Three", 1),
-    new Team("Regional Three", 2),
-    new Team("Regional Three", 3),
-    new Team("Regional Three", 4),
-    new Team("Regional Three", 5),
-    new Team("Regional Three", 6),
-    new Team("Regional Three", 7),
-    new Team("Regional Three", 8),
-    new Team("Regional Three", 9),
-    new Team("Regional Three", 10),
-    new Team("Regional Three", 11),
-    new Team("Regional Three", 12),
-    new Team("Regional Three", 13),
-    new Team("Regional Three", 14),
-    new Team("Regional Three", 15),
-    new Team("Regional Three", 16),
+    new Team("USC", 1),
+    new Team("Ohio State", 2),
+    new Team("UConn", 3),
+    new Team("Virginia Tech", 4),
+    new Team("Baylor", 5),
+    new Team("Syracuse", 6),
+    new Team("Duke", 7),
+    new Team("Kansas", 8),
+    new Team("Michigan", 9),
+    new Team("Richmond", 10),
+    new Team("AUB/ARIZ", 11),
+    new Team("Van/COLU", 12),
+    new Team("Marshall", 13),
+    new Team("Jackson St", 14),
+    new Team("Maine", 15),
+    new Team("Texas A&M-CC", 16),
 };
 var regional3Winner = BracketWinner(regional3Teams)[0];
 Console.WriteLine($"Regional Three Winner: {regional3Winner}");
 
 var regional4Teams = new Team[] {
-    new Team("Regional Four", 1),
-    new Team("Regional Four", 2),
-    new Team("Regional Four", 3),
-    new Team("Regional Four", 4),
-    new Team("Regional Four", 5),
-    new Team("Regional Four", 6),
-    new Team("Regional Four", 7),
-    new Team("Regional Four", 8),
-    new Team("Regional Four", 9),
-    new Team("Regional Four", 10),
-    new Team("Regional Four", 11),
-    new Team("Regional Four", 12),
-    new Team("Regional Four", 13),
-    new Team("Regional Four", 14),
-    new Team("Regional Four", 15),
-    new Team("Regional Four", 16),
+    new Team("Texas", 1),
+    new Team("Stanford", 2),
+    new Team("NC State", 3),
+    new Team("Gonzaga", 4),
+    new Team("Utah", 5),
+    new Team("Tennessee", 6),
+    new Team("Iowa State", 7),
+    new Team("Alabama", 8),
+    new Team("Florida St", 9),
+    new Team("Maryland", 10),
+    new Team("Green Bay", 11),
+    new Team("S Dakota St", 12),
+    new Team("UC Irvine", 13),
+    new Team("Chattanooga", 14),
+    new Team("Norfolk St", 15),
+    new Team("Drexel", 16),
 };
 var regional4Winner = BracketWinner(regional4Teams)[0];
 Console.WriteLine($"Regional Four Winner: {regional4Winner}");
@@ -127,7 +149,7 @@ var finalFour = new Team[] {
     regional3Winner,
     regional4Winner,
 };
-var champion = BracketWinner(finalFour)[0];
+var champion = BracketWinner(finalFour, true)[0];
 Console.WriteLine($"Champion: {champion}");
 
 public class Team(string name, int seed)
